e820f3161822ee7efe0fdfcc1973f54d7b136edb
Diego <alvarodiego@gmail.com>
2021-01-21 17:22:49 -0300

    Adicionada atualização para pagamento à vista

--- src/Entities/Contrato.cs
+++ src/Entities/Contrato.cs
@@ -29,6 +29,18 @@ namespace src.Entities
             sb.AppendLine($"Data: {Data.ToString("dd/MM/yyyy")}");
             sb.AppendLine($"Valor: R$ {Valor}");
             sb.AppendLine();
+            if(Fatura.Parcelas > 0)
+            {
+                sb.AppendLine($"Número de parcelas: {Fatura.Parcelas}");
+            }
+            else
+            {
+                sb.AppendLine("Pagamento à vista");
+            }
+            sb.AppendLine($"Tarifas totais: R$ {Fatura.Tarifas.ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Juros totais: R$ {Fatura.Juros.ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Pagamento total: R$ {Fatura.Total.ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.AppendLine();
             sb.AppendLine($"Vencimentos");
             sb.AppendLine(Fatura.ToString());
             return sb.ToString();
